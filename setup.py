3d3993aed9fd52507d60f87cf5ee2ded08729a46
Dushyant kumar verma <91840332+dushyantver@users.noreply.github.com>
2024-02-08 20:40:07 +0530

    name updatet

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
 
 __version__ = "0.0.1"
 REPO_NAME = "mangodb_package"
-PKG_NAME= "data_base_automation"
+PKG_NAME= "automation_mgd"
 AUTHOR_USER_NAME = "dushyantver"
 AUTHOR_EMAIL = "dushyantdchss@gmail.com"
 
